aa7db6053b9d4937469820d4e61066a288dd2838
Thorsten Heissel <thorsten.heissel@entarena.com>
2014-05-08 09:29:32 +0200

    Erstes .js mit erster Animation
    Die ersten events werden auf die Buttons gelegt und Erzeugen einer
    ersten Animation

--- js/th-modal-main.js
+++ js/th-modal-main.js
@@ -1,1 +1,22 @@
-console.log("th-Main");
+$(function() {
+
+  var is_touch_device = function() {
+    return !!('ontouchstart' in window);
+  };
+
+//Beschleunigt die Reaktionszeit auf Touch-Devices
+  var click_event = (is_touch_device()) ? "touchstart" : "click";  
+
+  var openModalHandler = function() {
+    $("#th-modal").addClass("th-show");
+  }
+
+  var closeModalHandler = function() {
+    $("#th-modal").removeClass("th-show");
+  }
+
+  $("#th-start-modal-1").on(click_event, openModalHandler);
+  $(".th-close").on(click_event, closeModalHandler);
+  $("#th-overlay").on(click_event, closeModalHandler);
+
+});
